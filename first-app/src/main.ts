c28d1dc2a00a84ce0d3ec78d8c7c417fcc35b3b1
Manassarn Manoonchai <manassarn@gmail.com>
2023-05-07 18:52:42 +0700

    Add router & route to details view

--- first-app/src/main.ts
+++ first-app/src/main.ts
@@ -1,10 +1,15 @@
 /*
-*  Protractor support is deprecated in Angular.
-*  Protractor is used in this example for compatibility with Angular documentation tools.
-*/
-import { bootstrapApplication,provideProtractorTestingSupport } from '@angular/platform-browser';
+ *  Protractor support is deprecated in Angular.
+ *  Protractor is used in this example for compatibility with Angular documentation tools.
+ */
+import {
+  bootstrapApplication,
+  provideProtractorTestingSupport,
+} from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import routeConfig from './app/routes';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent,
-    {providers: [provideProtractorTestingSupport()]})
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
